9e102b0597fc023c7c983176807fe3cc4ae58f40
manikamakam <abhibanerjee2494@gmail.com>
2020-02-21 21:04:53 -0500

    Updated input method to main file

--- Main.cpp
+++ Main.cpp
@@ -10,8 +10,27 @@ using namespace std;
 
 
 
-int main() {
-	vector<int> puzzle{1,2,3,0,4,5,8,6,7};
+int main(int argc, char* argv[]) {
+//	vector<int> puzzle{1,2,3,0,4,5,8,6,7};
+	vector<int> puzzle;
+	int ctr=1;
+	while(ctr!=10){
+		int r;
+		cout<<"Enter element "<<ctr;
+		std::cin>>r;
+		puzzle.push_back(r);
+		ctr +=1;	
+	}
+/*
+	char trem[9];
+	for(int i=0; i<argc;i++){
+		trem[i] = *argv[i];
+	}
+	vector<int> puzzle;
+	for(auto i: trem){
+		puzzle.push_back(int(i));	
+	}
+*/		
 	Node prnt = Node(puzzle, NULL);
 	prnt.id = -1;
 	Node root = Node(puzzle, &prnt);
